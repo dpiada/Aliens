f7d3e0f03e2ca10eb2b347d4aa5f5af46611b142
Daniele Piantoni <daniele.piantoni@digitiamo.com>
2022-09-17 07:59:43 +0200

    fix fire bullets

--- alien_invasion.py
+++ alien_invasion.py
@@ -3,7 +3,7 @@ import pygame
 
 from settings import Settings
 from ship import Ship
-from bullets import Bullets
+from bullet import Bullet
 
 class AlienInvasion:
 
@@ -17,8 +17,14 @@ class AlienInvasion:
     
     def _fire_bullet(self):
         """Create a new bullet and add it to the bullets group."""
-        new_bullet = Bullets(self)
+        new_bullet = Bullet(self)
         self.bullets.add(new_bullet)
+
+    def _update_bullets(self):
+        self.bullets.update()
+        for bullet in self.bullets.copy():
+            if bullet.rect.bottom <= 0:
+                self.bullets.remove(bullet)
 
     def _check_events(self):
         """Respond to keypresses and mouse events."""
@@ -37,7 +43,7 @@ class AlienInvasion:
                     self.ship.moving_up = True
                 elif event.key == pygame.K_SPACE:
                     self._fire_bullet()
-                    
+
             elif event.type == pygame.KEYUP:
                 if event.key == pygame.K_RIGHT:
                     self.ship.moving_right = False
@@ -51,20 +57,17 @@ class AlienInvasion:
     def _update_screen(self):
         self.screen.fill(self.settings.bg_color)
         self.ship.blitme()
-        pygame.display.flip()
         for bullet in self.bullets.sprites():
             bullet.draw_bullet()
+        pygame.display.flip()
+
 
     def run_game(self):
         while True:
             self._check_events()
             self.ship.update()
-            self.bullets.update()
+            self._update_bullets()
             self._update_screen()
-            for bullet in self.bullets.copy():
-                if bullet.rect.bottom <= 0:
-                    self.bullets.remove(bullet)
-    
 
 if __name__ == '__main__':
     # Make a game instance, and run the game.
