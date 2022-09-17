6160e9cc50f9d5a603b24b38dae407bb38d2f2ce
Daniele Piantoni <daniele.piantoni@digitiamo.com>
2022-09-17 09:21:45 +0200

    add Aliens and direction of alien

--- settings.py
+++ settings.py
@@ -15,3 +15,9 @@ class Settings:
         self.bullet_width = 3
         self.bullet_height = 15
         self.bullet_color = (0, 0, 0)
+
+        # Alien settings
+        self.alien_speed = 0.1
+        self.fleet_drop_speed = 5
+        # fleet_direction of 1 represents right; -1 represents left.
+        self.fleet_direction = 1
